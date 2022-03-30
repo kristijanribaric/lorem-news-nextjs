d9994b0e4ce752d2fbdedc604d677bc6f6744429
kristijanribaric <kriba13@gmail.com>
2022-03-30 22:33:21 +0200

    turned off github verf

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,31 +10,18 @@ export default authHandler;
 
 const options = {
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    // GitHubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // authorization: {
-      //   params: {
-      //     prompt: "consent",
-      //     access_type: "offline",
-      //     response_type: "code"
-      //   }
-      // }
+
     }),
 
   ],
-  // callbacks: {
-  //   async signIn({ account, profile }) {
-  //     if (account.provider === "google") {
-  //       return profile.email_verified && profile.email.endsWith("@example.com")
-  //     }
-  //     return true // Do different verification for other providers that don't have `email_verified`
-  //   },
-  // },
+
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
 };
