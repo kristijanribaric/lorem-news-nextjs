55f97c951b2f847e07f027f16b81df1ac1e5a0e4
kristijanribaric <kriba13@gmail.com>
2022-03-30 22:51:52 +0200

    brought back google verf

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,11 +11,11 @@ const options = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID ,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
-    // }),
+    }),
 
   ],
 
