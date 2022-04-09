f84710940d0e70f5ad36ccbbc989f07ca632e8ab
kristijanribaric <kriba13@gmail.com>
2022-04-09 11:49:18 +0200

    fetching images from the bucket

--- components/Article.tsx
+++ components/Article.tsx
@@ -6,7 +6,7 @@ const Article = ({id,title,image,author}) => {
   return (
     <Link href='/article/[id]' as={`/article/${id}`} passHref>
         <div className='cursor-pointer'>
-        <Image src={`/imgs/${image}`} alt={image} placeholder="blur" layout="responsive" width={170} height={100} sizes="50vw"  blurDataURL={`/imgs/${image}`} />
+        <Image src={image} alt={title} placeholder="blur" layout="responsive" width={170} height={100} sizes="50vw"  blurDataURL={image} />
         <h1 className='font-bold text-xl'>{title}</h1>
         <p className='font-thin text-gray-600'>{author}</p>
         </div>
