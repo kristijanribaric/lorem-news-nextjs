3c101082067ed11695979d1b4aa70fbccf040d6c
kristijanribaric <kriba13@gmail.com>
2022-04-11 14:20:23 +0200

    adjusted image blur

--- components/Article.tsx
+++ components/Article.tsx
@@ -6,7 +6,7 @@ const Article = ({id,title,image,author}) => {
   return (
     <Link href='/article/[id]' as={`/article/${id}`} passHref>
         <div className='cursor-pointer'>
-        <Image src={image} alt={title} placeholder="blur" layout="responsive" width={170} height={100} sizes="50vw"  blurDataURL={image} />
+        <Image src={image} alt={title} placeholder="blur" layout="responsive" width={170} height={100} sizes="50vw"/>
         <h1 className='font-bold text-xl'>{title}</h1>
         <p className='font-thin text-gray-600'>{author}</p>
         </div>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['qjdvptizvdiwobkicaon.supabase.co'],
+  },
 }
 
 module.exports = nextConfig
