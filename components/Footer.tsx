63b100a2c4db72e023f3dbf6c3b2f01432b11ea7
kristijanribaric <kriba13@gmail.com>
2022-04-29 20:06:12 +0200

    footer touchup

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='border-t-2 md:grid md:grid-cols-3 w-[95%]  m-auto'>
         <div className="flex items-center justify-center my-4">
         <Image src='/news_logo_red.png' alt='lorem news logo' width={60} height={60}/>
-        <h2 className="text-3xl font-semibold ml-4">Lorem <span className=" font-semibold">News</span></h2>
+        <h2 className="text-3xl font-semibold ml-4">Lorem <span className=" font-semibold text-red-600">News</span></h2>
         </div>
         <div className="flex justify-center gap-8 text-gray-500 my-4  items-center">
             <Link passHref href="/"><a className="hover:text-gray-300">Contact</a></Link>
